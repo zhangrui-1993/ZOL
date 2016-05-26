e618c8b47099349fb6c31178af768ca4d77b7763
zhangrui-1993 <664726168@qq.com>
2016-05-26 22:29:16 +0800

    5-26新增

--- js/index.js
+++ js/index.js
@@ -4,12 +4,12 @@ lunbo();
 function lunbo(){
 	$(".main .list li").eq(0).css({"background":"#c40000","color":"#fff"});
 	setInterval(function(){
-		$(".main .banner img").eq(index).slideUp(2000).css("top","2600px");
-//		$(".main .banner img").eq(index).siblings()
 		$(".main .list li").eq(index).css({"background":"#c40000","color":"#fff",}).siblings().css({"background":"#000","color":"#fff"});
+		$(".main .banner img").eq(index).show().animate({left:"0",},500).slideUp(500).next().show();
+//		animate({height:"0",},500);slideUp(500)
 		index++;
 		if(index == 6){
 			index = 0;
 		}
-	},2000);
+	},1000);
 }
