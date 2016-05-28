90e419fb4fae2b753d0a846eda4533e56e2f8728
zhangrui-1993 <664726168@qq.com>
2016-05-28 18:01:13 +0800

    5-28

---
+++ js/register.js
@@ -0,0 +1,11 @@
+//定义表单变量
+//手机号验证
+/*function phoneCode(){
+		var reg = /^[1][3578]\d{9}$/;
+	$(".tet1").blur(function(){
+		console.log(this.innerHTML);
+//		alert($(b));
+		
+	})
+}*/
+console.log($("form")[0])
